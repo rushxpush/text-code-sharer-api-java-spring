e84b2cc6fd63ecc741b0baa72b8fdfb528dd1466
rushxpush <rushxpush@gmail.com>
2025-01-11 01:23:37 -0300

    chore: install Lombok and refactor Document.java to remove getters and setters

--- src/main/java/com/textcodesharer/text_code_sharer/document/Document.java
+++ src/main/java/com/textcodesharer/text_code_sharer/document/Document.java
@@ -2,13 +2,20 @@ package com.textcodesharer.text_code_sharer.document;
 
 import java.util.Arrays;
 
-import jakarta.annotation.Nonnull;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 public class Document {
 	
@@ -27,85 +34,6 @@ public class Document {
 	private String password;
 	private boolean isGuest; 
 	
-	Document() {}
-	
-	Document(String title, String category, String content) {
-		this.title = title;
-		this.category = category;
-		this.content = content;
-	}
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getTitle() {
-		return title;
-	}
-
-	public void setTitle(String title) {
-		this.title = title;
-	}
-
-	public String getContent() {
-		return content;
-	}
-
-	public void setContent(String content) {
-		this.content = content;
-	}
-
-	public String getCategory() {
-		return category;
-	}
-
-	public void setCategory(String category) {
-		this.category = category;
-	}
-
-	public String getExpiration() {
-		return expiration;
-	}
-
-	public void setExpiration(String expiration) {
-		this.expiration = expiration;
-	}
-
-	public String getExposure() {
-		return exposure;
-	}
-
-	public void setExposure(String exposure) {
-		this.exposure = exposure;
-	}
-
-	public String[] getTags() {
-		return tags;
-	}
-
-	public void setTags(String[] tags) {
-		this.tags = tags;
-	}
-
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
-
-	public boolean isGuest() {
-		return isGuest;
-	}
-
-	public void setGuest(boolean isGuest) {
-		this.isGuest = isGuest;
-	}
 
 	@Override
 	public String toString() {
@@ -113,6 +41,4 @@ public class Document {
 				+ ", expiration=" + expiration + ", exposure=" + exposure + ", tags=" + Arrays.toString(tags)
 				+ ", isGuest=" + isGuest + "]";
 	}
-	
-
 }
